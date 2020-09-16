d2177faf438166090920c849c515ce015c2a8ea3
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2020-09-16 16:04:18 -0300

    Add contact page using react bootstrap

--- src/pages/Contacts/Add/index.js
+++ src/pages/Contacts/Add/index.js
@@ -4,193 +4,209 @@ import React, { useState } from "react";
 //	Importing React Router features
 import { Link, useHistory } from "react-router-dom";
 
+//	Importing React Bootstrap features
+import { Toast, Jumbotron, Col, Row, Image, Button, Card, Accordion, Form } from "react-bootstrap";
+
 //	Importing api to communicate to backend
 import api from "../../../services/api";
 
-//	Importing page styles
-import "./styles.css";
+// Importing default avatar
+import avatar from "../../../assets/avatar.png";
 
 //	Exporting resource to routes.js
 export default function Add() {
 	//	Setting background style properties
 	document.getElementsByTagName("body")[0].style = "backdrop-filter: blur(4px)";
-	
+
 	//  Defining state variables
 	const [name, setName] = useState("");
 	const [surname, setSurname] = useState("");
-	const [telephone, setTelephone] = useState("");
+	const [phone, setPhone] = useState("");
 	const [email, setEmail] = useState("");
 	const [address, setAddress] = useState("");
 	const [annotations, setAnnotations] = useState("");
 	const imageName = "";
-	
+
+	//	Message settings
+	const [toastShow, setToastShow] = useState(false);
+	const [title, setTitle] = useState("");
+	const [message, setMessage] = useState("");
+
 	//	Defining history to jump through pages
 	const history = useHistory();
 
 	// Function to handle contact adding
-	async function handleAddContact(e) {
-		e.preventDefault();
+	async function handleAddContact(event) {
+		event.preventDefault();
 
-		try {
-			const response = await api.post("contacts", { 
-				name, 
-				surname, 
-				telephone, 
-				email, 
-				address, 
-				annotations, 
-				imageName 
-			}, {
-				headers: {
-					Authorization: sessionStorage.getItem("userId")
-				}
-			});
-
+		await api.post("contacts", {
+			name,
+			surname,
+			phone,
+			email,
+			address,
+			annotations,
+			imageName
+		}, {
+			headers: {
+				Authorization: sessionStorage.getItem("userId")
+			}
+		}).then(() => {
 			history.push("/contacts");
-			
-			global.message = response.data;
-		} catch(error) {
-			alert(error.response.data);
-		}
+		}).catch((error) => {
+			setTitle("Erro!");
+			setMessage(error.response ? error.response.data : error.message);
+			setToastShow(true);
+		});
 	}
 
-	//	Testing if user is logged in
-	if(sessionStorage.getItem("userId")) {
-		return (
-			<div className="contact-container">
-				<div className="col-md-7 jumbotron p-4 m-auto">
-					<h3>Novo contato:</h3>
-					<form onSubmit={handleAddContact}>
-						<div className="row my-1">
-							<div className="col-md text-center">
-								<img 
-									className="img-thumbnail rounded" 
-									id="avatar" 
-									alt="Avatar" 
-									src="/avatar.png"
-								/>
-							</div>
-							<div className="col-md d-flex flex-column justify-content-between">
-								<div className="row mb-1">
-									<div className="col">
-										<label>Nome: </label>
-										<input 
-											type="text" 
-											className="form-control" 
-											name="name" 
-											placeholder="Nome" 
+	const toast = (
+		<div
+			aria-live="polite"
+			aria-atomic="true"
+			style={{
+				position: "fixed",
+				top: "7%",
+				right: "2%",
+				zIndex: 5
+			}}
+		>
+			<Toast show={toastShow} onClose={() => setToastShow(false)} delay={3000} autohide>
+				<Toast.Header>
+					<strong className="mr-auto">{title}</strong>
+				</Toast.Header>
+				<Toast.Body>{message}</Toast.Body>
+			</Toast>
+		</div>
+	);
+
+	return (
+		<div className="contact-container d-flex h-100">
+			{toast}
+			<Jumbotron className="col-sm-7 p-4 m-auto">
+				<h3>Novo contato:</h3>
+				<Form onSubmit={handleAddContact}>
+					<Row>
+						<Col className="text-center" sm>
+							<Image
+								id="avatar"
+								alt="Avatar"
+								src={avatar}
+								fluid
+								rounded
+							/>
+						</Col>
+						<Col className="d-flex flex-column justify-content-between" sm>
+							<Row>
+								<Col>
+									<Form.Group controlId="name">
+										<Form.Label>Nome</Form.Label>
+										<Form.Control
+											placeholder="Nome"
+											type="text"
 											value={name}
-											onChange={(e) => setName(e.target.value)}
+											onChange={e => setName(e.target.value)}
 											required
 										/>
-									</div>
-								</div>
-								<div className="row my-1">
-									<div className="col">
-										<label>Sobrenome: </label>
-										<input 
-											type="text" 
-											className="form-control" 
-											name="surname" 
+									</Form.Group>
+								</Col>
+							</Row>
+							<Row>
+								<Col>
+									<Form.Group controlId="surname">
+										<Form.Label>Sobrenome</Form.Label>
+										<Form.Control
 											placeholder="Sobrenome"
+											type="text"
 											value={surname}
-											onChange={(e) => setSurname(e.target.value)}
+											onChange={e => setSurname(e.target.value)}
 										/>
-									</div>
-								</div>
-								<div className="row mt-1">
-									<div className="col">
-										<label>Telefone: </label>
-										<input 
-											type="tel" 
-											className="form-control" 
-											name="telephone" 
-											pattern="\([0-9]{2}\) [0-9]{4,5}-[0-9]{4}$" 
-											placeholder="(DDD) 9xxxx-xxxx"
-											value={telephone}
-											onChange={(e) => setTelephone(e.target.value)}
+									</Form.Group>
+								</Col>
+							</Row>
+							<Row>
+								<Col>
+									<Form.Group controlId="phone">
+										<Form.Label>Telefone</Form.Label>
+										<Form.Control
+											placeholder="Telefone"
+											type="text"
+											value={phone}
+											onChange={e => setPhone(e.target.value)}
 										/>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="row my-1" id="accordion">
-							<div className="jumbotron p-0 my-2 w-100">
-								<div className="card-header" id="headingOne">
-									<h5 className="mb-0">
-										<button type="button" className="btn btn-outline-light btn-block" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-											Adicionar mais informações
-										</button>
-									</h5>
-								</div>
-								<div id="collapseOne" className="collapse" aria-labelledby="headingOne" data-parent="#accordion">
-									<div className="card-body m-0 p-4">
-										<div className="row">
-											<div className="col-md d-flex flex-column justify-content-between">
-												<div className="row my-1">
-													<div className="col">
-														<label>Email: </label>
-														<input 
-															type="email" 
-															className="form-control" 
-															name="email" 
-															placeholder="Email"
-															value={email}
-															onChange={(e) => setEmail(e.target.value)}
-														/>
-													</div>
-												</div>
-												<div className="row my-1">
-													<div className="col">
-														<label>Endereço: </label>
-														<input 
-															type="text" 
-															className="form-control" 
-															name="address" 
-															placeholder="Endereço"
-															value={address}
-															onChange={(e) => setAddress(e.target.value)}
-														/>
-													</div>
-												</div>
-											</div>
-											<div className="col-md">
-												<label>Anotações: </label>
-												<textarea 
-													className="form-control" 
-													name="annotations" 
-													style={{ resize: "none" }}
-													maxLength="200" 
-													cols="15" 
-													rows="4"
+									</Form.Group>
+								</Col>
+							</Row>
+						</Col>
+					</Row>
+					<Jumbotron className="p-0">
+						<Accordion className="w-100">
+							<Card className="w-100" bg="transparent">
+								<Card.Header className="text-center w-100">
+									<Accordion.Toggle as={Button} variant="outline-light" eventKey="0">
+										Adicionar mais informações
+									</Accordion.Toggle>
+								</Card.Header>
+								<Accordion.Collapse eventKey="0">
+									<Card.Body>
+										<Row className="row">
+											<Col sm>
+												<Row>
+													<Col>
+														<Form.Group controlId="email">
+															<Form.Label>Email</Form.Label>
+															<Form.Control
+																placeholder="Email"
+																type="email"
+																value={email}
+																onChange={e => setEmail(e.target.value)}
+															/>
+														</Form.Group>
+													</Col>
+												</Row>
+												<Row>
+													<Col>
+														<Form.Group controlId="address">
+															<Form.Label>Endereço</Form.Label>
+															<Form.Control
+																placeholder="Endereço"
+																type="text"
+																value={address}
+																onChange={e => setAddress(e.target.value)}
+															/>
+														</Form.Group>
+													</Col>
+												</Row>
+											</Col>
+											<Col sm>
+												<Form.Control
 													value={annotations}
-													onChange={(e) => setAnnotations(e.target.value)}
+													onChange={e => setAnnotations(e.target.value)}
+													placeholder="Anotações"
+													as="textarea"
+													rows="6"
+													style={{ resize :"none" }}
 												/>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className="row justify-content-end m-0">
-							<input 
-								type="submit" 
-								className="btn btn-success mx-3" 
-								id="submitButton" 
-								value="Cadastrar"
-							/>
-							<Link className="btn btn-secondary mx-3" to="/contacts">
-								Cancelar
-							</Link>
-						</div>
-					</form>
-				</div>
-			</div>
-		);
-	} else {
-		alert("You must log in before accessing this page!");
-		history.push("/user/login");
-
-		return null;
-	}
+											</Col>
+										</Row>
+									</Card.Body>
+								</Accordion.Collapse>
+							</Card>
+						</Accordion>
+					</Jumbotron>
+					<Row className="justify-content-end m-0">
+						<Button
+							type="submit"
+							variant="success"
+						>
+							Cadastrar
+						</Button>
+						<Link className="btn btn-secondary mx-3" to="/contacts">
+							Cancelar
+						</Link>
+					</Row>
+				</Form>
+			</Jumbotron>
+		</div>
+	);
 }
