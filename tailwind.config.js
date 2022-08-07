6aa3f2be89a17936d75100c06887f06ef5b9c644
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2022-08-07 09:44:32 -0300

    Setting up styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
 	presets: [],
+	prefix: 'tw-',
 	darkMode: 'media', // or 'class'
 	theme: {
 		screens: {
