6a64b465d399a9ce5510eb9a54a206c0af894782
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2022-08-07 10:12:25 -0300

    Finishing footer

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -1,7 +1,14 @@
 function Footer() {
 	return (
-		<div>
-			<h1>Footer</h1>
+		<div className="tw-flex tw-flex-row tw-gap-x-1 tw-justify-center">
+			<p>{`Copyright © ${new Date().getFullYear()} | Built with ❤️ by`}</p>
+			<a
+				href="https://github.com/ThiagoPereiraUFV"
+				target="_blank"
+				rel="noreferrer"
+			>
+				Thiago Pereira
+			</a>
 		</div>
 	)
 }
