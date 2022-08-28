c281e8690decf613d18b841c745bf2352f3bc7d1
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2022-08-28 08:46:16 -0300

    Fixing default layout styles

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -4,11 +4,11 @@ import Navbar from '../components/organisms/Navbar'
 
 function Layout({ children }: React.PropsWithChildren<unknown>) {
 	return (
-		<>
+		<div className="tw-grid tw-grid-cols-1 tw-content-between tw-h-screen">
 			<Navbar />
 			<main>{children}</main>
 			<Footer />
-		</>
+		</div>
 	)
 }
 
