3df434b3ac82ffc1ac1f6cf70e841f54d8efd571
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2022-08-28 08:51:31 -0300

    Finishing home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,38 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import Jumbotron from '../components/organisms/Jumbotron'
 
 const Home: NextPage = () => {
-	return <h1>Home</h1>
+	const router = useRouter()
+	const jumbotronProps = {
+		title: {
+			text: 'EasyContacts',
+			className: 'tw-font-normal tw-text-white tw-mb-4',
+		},
+		subtitle: {
+			text: 'O seu gerenciador de contatos',
+			className: 'tw-text-white tw-mb-2',
+		},
+		body: {
+			text: 'Armazene seus contatos e leve-os para qualquer lugar com o EasyContacts. Abra agora uma conta e sincronize todos os seus contatos de forma rápida e gratuita.',
+			className: 'tw-text-white',
+		},
+		button: {
+			label: 'Acessar conta',
+			onClick: () => router.push('/user/login'),
+			className: '',
+		},
+	}
+
+	return (
+		<Jumbotron
+			title={jumbotronProps.title}
+			subtitle={jumbotronProps.subtitle}
+			body={jumbotronProps.body}
+			button={jumbotronProps.button}
+			className="tw-bg-purpleTransparent tw-rounded-3xl tw-px-8 tw-py-12 tw-w-2/3 tw-ml-16"
+		/>
+	)
 }
 
 export default Home
