ebc9f2bc829a686746cf44107fd8fc1a91efb791
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2022-08-28 08:54:00 -0300

    Fixing footer styles

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -1,11 +1,12 @@
 function Footer() {
 	return (
-		<div className="tw-flex tw-flex-row tw-gap-x-1 tw-justify-center">
+		<div className="tw-flex tw-flex-row tw-gap-x-1 tw-justify-center tw-text-white tw-py-2">
 			<p>{`Copyright © ${new Date().getFullYear()} | Built with ❤️ by`}</p>
 			<a
 				href="https://github.com/ThiagoPereiraUFV"
 				target="_blank"
 				rel="noreferrer"
+				className="hover:tw-text-purpleTransparent"
 			>
 				Thiago Pereira
 			</a>
