5648f86b06e13681f3c954655aa60038e4f5797b
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2022-11-15 20:57:52 -0300

    feat(defaul): hiding navbar when session unexists

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,11 +1,14 @@
+import { useSession } from 'next-auth/react'
 import * as React from 'react'
 import Footer from '../components/organisms/Footer'
 import Navbar from '../components/organisms/Navbar'
 
 function Layout({ children }: React.PropsWithChildren<unknown>) {
+	const { data: session } = useSession()
+
 	return (
 		<div className="tw-grid tw-content-between tw-h-screen">
-			<Navbar />
+			{session?.jwt.length && <Navbar />}
 			{children}
 			<Footer className="tw-flex tw-flex-row tw-gap-x-1 tw-justify-center tw-text-white tw-py-2" />
 		</div>
