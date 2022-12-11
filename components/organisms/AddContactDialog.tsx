697f682dae251207a33c678c76b687722ea23368
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2022-12-10 21:17:06 -0300

    feat(AddContactDialog): implementing form on AddContactDialog

--- components/organisms/AddContactDialog.tsx
+++ components/organisms/AddContactDialog.tsx
@@ -3,12 +3,17 @@ import {
 	Dialog,
 	DialogActions,
 	DialogContent,
-	DialogContentText,
 	DialogTitle,
 	Slide,
+	TextareaAutosize,
+	TextField,
 } from '@mui/material'
 import { TransitionProps } from '@mui/material/transitions'
-import React from 'react'
+import api from 'helpers/api'
+import { useRouter } from 'next/router'
+import React, { ChangeEvent, useState } from 'react'
+import { useEffect } from 'react'
+import { IContact } from 'types/contact'
 
 const Transition = React.forwardRef(function Transition(
 	props: TransitionProps & {
@@ -26,23 +31,144 @@ interface AddContactDialog extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 function AddContactDialog({ className = '', open, setOpen }: AddContactDialog) {
+	const router = useRouter()
+	const [contact, setContact] = useState<IContact>({} as IContact)
+
+	useEffect(() => {
+		setContact({} as IContact)
+	}, [open])
+
+	async function handleSubmit() {
+		const { data } = await api.post('/contacts/mine', contact)
+
+		if (!data) {
+			throw new Error('Error creating contact')
+		}
+
+		router.reload()
+	}
+
 	return (
 		<Dialog
 			open={open}
 			TransitionComponent={Transition}
-			keepMounted
 			aria-describedby="alert-dialog-slide-description"
 			className={className}
+			maxWidth="md"
+			fullWidth
 		>
-			<DialogTitle>{"Use Google's location service?"}</DialogTitle>
-			<DialogContent>
-				<DialogContentText id="alert-dialog-slide-description">
-					Let Google help apps determine location. This means sending anonymous
-					location data to Google, even when no apps are running.
-				</DialogContentText>
+			<DialogTitle>Adicionar novo contato</DialogTitle>
+			<DialogContent className="tw-grid tw-grid-cols-1 lg:tw-grid-cols-2 tw-gap-4">
+				<TextField
+					id="name"
+					label="Nome"
+					type="text"
+					name="name"
+					variant="outlined"
+					color="primary"
+					className="tw-bg-white tw-rounded-full"
+					sx={{
+						'& fieldset': {
+							borderRadius: '30px',
+						},
+					}}
+					value={contact.name}
+					onChange={({ target }: ChangeEvent<HTMLInputElement>) =>
+						setContact({ ...contact, name: target.value })
+					}
+				/>
+				<TextField
+					id="surname"
+					label="Sobrenome"
+					type="text"
+					name="surname"
+					variant="outlined"
+					color="primary"
+					className="tw-bg-white tw-rounded-full"
+					sx={{
+						'& fieldset': {
+							borderRadius: '30px',
+						},
+					}}
+					value={contact.surname}
+					onChange={({ target }: ChangeEvent<HTMLInputElement>) =>
+						setContact({ ...contact, surname: target.value })
+					}
+				/>
+				<TextField
+					id="phone"
+					label="Telefone"
+					type="tel"
+					name="phone"
+					variant="outlined"
+					color="primary"
+					className="tw-bg-white tw-rounded-full"
+					sx={{
+						'& fieldset': {
+							borderRadius: '30px',
+						},
+					}}
+					value={contact.phone}
+					onChange={({ target }: ChangeEvent<HTMLInputElement>) =>
+						setContact({ ...contact, phone: target.value })
+					}
+				/>
+				<TextField
+					id="email"
+					label="Telefone"
+					type="email"
+					name="email"
+					variant="outlined"
+					color="primary"
+					className="tw-bg-white tw-rounded-full"
+					sx={{
+						'& fieldset': {
+							borderRadius: '30px',
+						},
+					}}
+					value={contact.email}
+					onChange={({ target }: ChangeEvent<HTMLInputElement>) =>
+						setContact({ ...contact, email: target.value })
+					}
+				/>
+				<TextField
+					id="address"
+					label="Endereço"
+					type="text"
+					name="address"
+					variant="outlined"
+					color="primary"
+					className="tw-bg-white tw-rounded-full"
+					sx={{
+						'& fieldset': {
+							borderRadius: '30px',
+						},
+					}}
+					value={contact.address}
+					onChange={({ target }: ChangeEvent<HTMLInputElement>) =>
+						setContact({ ...contact, address: target.value })
+					}
+				/>
+				<TextareaAutosize
+					id="annotations"
+					placeholder="Anotações"
+					name="annotations"
+					color="primary"
+					className="tw-bg-white tw-rounded-xl tw-border tw-border-gray-400 hover:tw-border-black tw-p-1"
+					minRows={5}
+					defaultValue={contact.annotations ?? ''}
+					onChange={({ target }: ChangeEvent<HTMLTextAreaElement>) =>
+						setContact({ ...contact, annotations: target.value })
+					}
+				/>
 			</DialogContent>
 			<DialogActions>
-				<Button onClick={() => setOpen(false)}>Disagree</Button>
+				<Button color="secondary" onClick={() => setOpen(false)}>
+					Cancelar
+				</Button>
+				<Button type="submit" color="primary" onClick={handleSubmit}>
+					Salvar
+				</Button>
 			</DialogActions>
 		</Dialog>
 	)
