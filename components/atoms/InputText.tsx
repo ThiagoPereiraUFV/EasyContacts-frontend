0f54ef8f0c0d9cd48d6ae192162361e41dc72e45
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2023-02-11 15:08:51 -0300

    feat: creating InputText component

---
+++ components/atoms/InputText.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react'
+import { TextField } from '@mui/material'
+
+interface InputTextProps extends React.PropsWithChildren<unknown> {
+	id: string
+	label: string
+	type?: React.HTMLInputTypeAttribute
+	className?: string
+	value: string
+	setter: React.Dispatch<React.SetStateAction<string>>
+}
+
+function InputText({
+	id,
+	label,
+	type = 'string',
+	className,
+	value,
+	setter,
+}: InputTextProps) {
+	return (
+		<TextField
+			id={id}
+			name={id}
+			label={label}
+			type={type}
+			variant="outlined"
+			color="primary"
+			className={className}
+			sx={{
+				'& fieldset': {
+					borderRadius: '30px',
+				},
+			}}
+			value={value}
+			onChange={({ target }: React.ChangeEvent<HTMLInputElement>) =>
+				setter(target.value)
+			}
+		/>
+	)
+}
+
+export default InputText
