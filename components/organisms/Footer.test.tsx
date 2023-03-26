b43dd8aed8b807a010586808209145cd021423d1
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2023-03-26 10:16:34 -0300

    feat: creating Footer unit test

---
+++ components/organisms/Footer.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import Footer from 'components/organisms/Footer'
+import '@testing-library/jest-dom'
+
+describe('Footer', () => {
+	it(`Renders a Footer with text: Thiago Pereira`, () => {
+		render(<Footer />)
+
+		const texts = screen.getAllByText('Thiago Pereira')
+
+		for (const text of texts) {
+			expect(text).toBeInTheDocument()
+		}
+	})
+})
